337e6734ce1cccc3a1d127742d50a4cdfdcdc410
ollegreen <olleg.green@gmail.com>
2024-07-02 20:33:11 +0100

    tinker

--- ml/transformer/transformer.py
+++ ml/transformer/transformer.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO,format='%(message)s')
 from hyperparameters import sentence
 
 from config import (
-    d_model,
+    d_model, # size of embedding vector
     vocab_size,
     dropout,
     seq_length,
@@ -120,25 +120,48 @@ MULTI-HEAD ATTENTION:
         K-vector * W^K_matrix = K'
         V-vector * W^v_matrix = V'
 
+        Then we split these matrices into smaller matrices (same number as the number of heads we want):
+            Q' = Q1, Q2, Q3, Q4 ... etc
+            K' = K1, K2, K3, K4 ... etc
+            V' = V1, V2, V3, V4 ... etc
+        
+        ^this split is done so that the Q1 matrix has the same dimension as the embedding dimension (d_model),
+        this gives each head the access to the full sentence, but 
+
         Then we take these smaller matrices and use the Attention formula:
         23 min: https://www.youtube.com/watch?v=ISNdQcPhsts&t=340s&ab_channel=UmarJamil
+
+        where we concatinate the heads:
+            MultiHead (Q,K,V) = Concat(head1, ...head_n_heads,) * W°
 
 """
 
 class MultiHeadAttentionBlock(nn.Module):
 
-    def __init__(self, d_model: int, n_heads: int, dropout: float) -> None:
+    def __init__(self, d_model: int, num_heads: int, dropout: float) -> None:
         super().__init__()
         self.d_model = d_model
-        self.n_heads = n_heads
-        # Check that d_model can be divisible by the number of heads:
-        assert d_model % n_heads == 0, "d_model is not divisible by the number of heads. Fix it mate."
+        self.num_heads = num_heads
+        # We need to divide the embedding vector by num_heads, so let's assert that:
+        assert d_model & num_heads == 0, "d_model is not divisible by num_heads"
+        self.d_k = d model // num_heads # dk = d_model / num_heads # TODO: what does this mean?
+        self.w_q = nn.Linear(d_model, d_model) # this is the W^Q_matrix
+        self.w_k = nn.Linear(d_model, d_model) # this is the W^K_matrix
+        self.w_v = nn.Linear(d_model, d_model) # this is the W^V_matrix
+        self.w_o = 
+        self.dropout = dropout
         
-        # d_model / number_of_heads -> gives us the dK value which is the final output before concatenating the K,Q,V.
-        self.d_k = d_model // n_heads
-
-        self.w_q = nn.Linear(d_model, d_model) # Wq
-        self.w_k = nn.Linear(d_model, d_model) # Wk
-        self.w_v = nn.Linear(d_model, d_model) # Wv
 
 
+
+
+
+
+
+
+
+
+
+
+
+                 
